58c5324b66a979fd00b6b16831b171280ab62431
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-06-01 04:31:53 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const path=require('path')
 const dotenv=require("dotenv")
 dotenv.config()
 const cookieParser=require('cookie-parser')
@@ -10,21 +11,25 @@ const messageRoutes=require('./routes/message')
 const userRoutes=require('./routes/userRouter')
 const { app,server } = require('./socket/socket')
 
+
 //middlewares
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(bodyParser())
 connectToMongoDb()
 
+var __dirname = path.resolve();
 
-
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*",(req,res)=>{
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT,()=>{
     console.log(`server is running on ${PORT} `)
