40646b1b73fd891664ca7512606f8faf8dc39dc5
Eyepatch5263 <pratyushpragyey@gmail.com>
2024-06-01 14:04:22 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,16 @@ app.use(cookieParser())
 app.use(bodyParser())
 connectToMongoDb()
 
-var __dirname = path.resolve();
 
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname,"../","frontend","dist")))
 
 app.get("*",(req,res)=>{
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname,"../","frontend","dist","index.html"));
 })
 
 server.listen(PORT,()=>{
